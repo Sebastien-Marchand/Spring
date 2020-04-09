32ceb154df9d6ae5cdcbf319ce313a118bf0fbda
Sébastien Marchand <sebastien.marchand78@gmail.com>
2020-04-09 16:38:14 +0200

    1e version TP 3.1 SpringTest

---
+++ src/main/java/PlatDaoMemoireTest.java
@@ -0,0 +1,29 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import dev.dao.PlatDaoMemoire;
+
+class PlatDaoMemoireTest {
+
+	private PlatDaoMemoire platDaoMemoire;
+	
+	@BeforeEach
+	void setUp() {
+		this.platDaoMemoire = new PlatDaoMemoire();
+	}
+
+	@Test
+	void LissterPlatsVideInitialisation() {
+		assertThat(platDaoMemoire.listerPlats()).isEmpty();
+	}
+	
+	@Test
+	void ajouterPlatCasPassants() {
+		
+		platDaoMemoire.ajouterPlat("Kebab", 500);
+		assertThat(platDaoMemoire.listerPlats()).isNotEmpty();
+	}
+
+}
